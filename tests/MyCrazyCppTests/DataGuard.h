d2ec26b60ebc4ea7a42d5306d2fe9f327d2e5e3d
Alexis CAILLY <a.cailly@i2s.fr>
2021-04-27 15:01:53 +0200

    Create a header a DataGuard

---
+++ tests/MyCrazyCppTests/DataGuard.h
@@ -0,0 +1,54 @@
+#pragma once
+
+
+template <typename T>
+struct CDataGuard
+{
+	std::shared_ptr<T> m_proc;
+
+	CDataGuard(std::shared_ptr<T>&& ptr) :
+		m_proc(ptr) { }
+
+	CDataGuard(std::shared_ptr<T>& ptr) :
+		m_proc(ptr) { }
+
+	auto operator->() {
+		return lock_guard();
+	}
+
+	auto operator->() const {
+		return lock_guard();
+	}
+
+	auto lock_guard() const {
+		return CLocker{ m_protector, m_proc.get() };
+	}
+
+	auto get() {
+		return m_proc.get();
+	}
+
+	struct CLocker
+	{
+		std::lock_guard<std::mutex> m_locker;
+		T* m_ptr;
+
+		template <typename Locker, typename Ptr>
+		CLocker(Locker&& lc, Ptr&& ptr) :
+			m_locker(std::forward<Locker>(lc)),
+			m_ptr(std::forward<Ptr>(ptr))
+		{
+			assert(ptr != nullptr && "invalid pointer to protect");
+		}
+
+		T* operator->() {
+			return m_ptr;
+		}
+		const T* operator->() const {
+			return m_ptr;
+		}
+	};
+
+protected:
+	mutable std::mutex m_protector;
+};
--- tests/MyCrazyCppTests/main.cpp
+++ tests/MyCrazyCppTests/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <type_traits>
 #include <string>
@@ -7,58 +6,7 @@
 #include <mutex>
 #include <memory>
 #include <vector>
-
-template <typename T>
-struct CDataGuard
-{
-	std::shared_ptr<T> m_proc;
-
-	CDataGuard(std::shared_ptr<T>&& ptr) :
-		m_proc(ptr) { }
-
-	CDataGuard(std::shared_ptr<T>& ptr) :
-		m_proc(ptr) { }
-
-	auto operator->() {
-		return lock_guard();
-	}
-
-	auto operator->() const {
-		return lock_guard();
-	}
-
-	auto lock_guard() const {
-		return CLocker{ m_protector, m_proc.get() };
-	}
-
-	auto get() {
-		return m_proc.get();
-	}
-
-	struct CLocker
-	{
-		std::lock_guard<std::mutex> m_locker;
-		T* m_ptr;
-
-		template <typename Locker, typename Ptr>
-		CLocker(Locker&& lc, Ptr&& ptr) :
-			m_locker(std::forward<Locker>(lc)),
-			m_ptr(std::forward<Ptr>(ptr))
-		{
-			assert(ptr != nullptr && "invalid pointer to protect");
-		}
-
-		T* operator->() {
-			return m_ptr;
-		}
-		const T* operator->() const {
-			return m_ptr;
-		}
-	};
-
-protected:
-	mutable std::mutex m_protector;
-};
+#include "DataGuard.h"
 
 
 struct Foo
@@ -84,6 +32,7 @@ int main(int argc, char* argv[])
 	CDataGuard myprotectedData = std::make_shared<Foo>();
 
 	std::thread th1 = std::thread([&] {
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 		myprotectedData->f1();
 	});
 
@@ -92,6 +41,7 @@ int main(int argc, char* argv[])
 	});
 
 	std::thread th3 = std::thread([&] {
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 		auto locker = myprotectedData.lock_guard();
 		locker->f2();
 		locker->f1();
@@ -101,9 +51,12 @@ int main(int argc, char* argv[])
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	myprotectedData->f1();
-
+	
 	th2.join();
+
 	myprotectedData->f2();
+	
 	th3.join();
+
 	return EXIT_SUCCESS;
 }
