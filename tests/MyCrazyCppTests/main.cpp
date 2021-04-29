305a4249a1c5fc15a630e98d117e7e377664dd53
Alexis CAILLY <a.cailly@i2s.fr>
2021-04-29 17:19:28 +0200

    Add some setter/getter

--- tests/MyCrazyCppTests/main.cpp
+++ tests/MyCrazyCppTests/main.cpp
@@ -1,57 +1,14 @@
 #include <iostream>
+#include <string>
 
-
-#include "DataGuard.h"
-
-
-struct Foo
-{
-	void f1() {
-		std::cout << "f1 wait: " << std::this_thread::get_id();
-		std::this_thread::sleep_for(std::chrono::seconds(2));
-		std::cout << " - end" << std::endl;
-	}
-
-	void f2() const {
-		std::cout << "f2 wait: " << std::this_thread::get_id();
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-		std::cout << " - end" << std::endl;
-	}
-};
-
-
+#include "SharedGuard.h"
 
 int main(int argc, char* argv[])
 {
-
-	CDataGuard myprotectedData = std::make_shared<Foo>();
-
-	std::thread th1 = std::thread([&] {
-		std::this_thread::sleep_for(std::chrono::milliseconds(500));
-		myprotectedData->f1();
-	});
-
-	std::thread th2 = std::thread([&] {
-		myprotectedData->f2();
-	});
-
-	std::thread th3 = std::thread([&] {
-		std::this_thread::sleep_for(std::chrono::milliseconds(500));
-		auto locker = myprotectedData.lock_guard();
-		locker->f2();
-		locker->f1();
-	});
-
-	th1.join();
-
-	std::this_thread::sleep_for(std::chrono::milliseconds(500));
-	myprotectedData->f1();
 	
-	th2.join();
-
-	myprotectedData->f2();
-	
-	th3.join();
+	unit_test::samethreadlock_test();
+	unit_test::access_test();
+	unit_test::multithread_test();
 
 	return EXIT_SUCCESS;
 }
