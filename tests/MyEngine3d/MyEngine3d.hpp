c90a7bfacfb33485dc1aa83092d3624afe2cba02
Alexis CAILLY <a.cailly@i2s.fr>
2021-09-15 15:49:21 +0200

    Ajout tests

--- tests/MyEngine3d/MyEngine3d.hpp
+++ tests/MyEngine3d/MyEngine3d.hpp
@@ -4,6 +4,8 @@
 #include "BH3D_SDLImGUI.hpp"
 #include "BH3D_GLCheckError.hpp"
 #include "BH3D_Cube.hpp"
+#include "BH3D_Sphere.hpp"
+#include "BH3D_Cylinder.hpp"
 #include "BH3D_TinyShader.hpp"
 #include "BH3D_Drawable.hpp"
 #include "BH3D_TexturePerlin.hpp"
@@ -16,10 +18,75 @@ class MyEngine3d : public bh3d::SDLEngine
 	bh3d::SDLImGUI m_imGUI;
 	bh3d::Observer m_observer = { *this };
 	bh3d::Drawable m_cube;
+	bh3d::Drawable m_triangle;
+	bh3d::Drawable m_multiMeshes;
+
 public:
 	MyEngine3d(const bh3d::WindowInfo& windowInfo = {}) :
 		SDLEngine(windowInfo)
 	{		}
+
+	void InitTriangle()
+	{
+		
+		bh3d::Mesh& mesh = m_triangle.m_mesh; //m_triangle -> Drawable (mesh + shader)
+
+		//Colored Triangle initialization
+		auto vertexs = std::vector<glm::vec3>{
+			{-1.0f, 0.0f, 0.0f},
+			{1.0f, 0.0f, 0.0f},
+			{0.0f, 1.0f, 0.0f}
+		};
+
+		auto colors = std::vector<glm::vec4>{
+			{1.0f, 0.0f, 0.0f, 1.0f},    //red
+			{0.0f, 1.0f, 0.0f, 1.0f},	 //green
+			{0.0f, 0.0f, 1.0f, 1.0f}	 //blue
+		};
+
+		auto faces = std::vector<bh3d::Face>{
+			{0,1,2}	// a single triangle with vertex indexes [0,1,2]
+		};
+
+		mesh.AddColoredSubMesh(faces, vertexs, colors);		//Add a sub-mesh 0
+		mesh.AddColoredSubMesh(faces, vertexs, colors);		//Add a sub-mesh 1
+		mesh.AddColoredSubMesh(faces, vertexs, colors);		//Add a sub-mesh 2
+
+		mesh.TranslateMesh(glm::vec3{ -1.0f,0.0f,0.0f }, 1);
+		mesh.TranslateMesh(glm::vec3{ 1.0f,0.5f,0.0f }, 2);
+
+		mesh.ComputeMesh();		// Send to the GPU
+		mesh.FreeArraysCPU();	// Free CPU Array
+
+		// colored shader (used to draw the triangle)
+		bh3d::Shader& shader = m_triangle.m_shader;
+		shader.LoadRaw(bh3d::TinyShader::COLOR_VERTEX(), bh3d::TinyShader::COLOR_FRAGMENT());
+
+	}
+
+	void InitMultiMeshes()
+	{
+		bh3d::Mesh& mesh = m_multiMeshes.m_mesh; //m_triangle -> Drawable (mesh + shader)
+
+		bh3d::Texture texture = bh3d::TexturePerlin::CreateRGB(256, 256, 6.0f, 1.0f, true);
+
+		bh3d::Sphere::AddSubMesh(mesh);		//sphere radius 1
+		bh3d::Cube::AddSubMesh(mesh);		//box size 1
+		bh3d::Cube::AddSubMesh(mesh);		//box size 1
+
+		mesh.TranslateMesh(glm::vec3{ -1.0f,0.0f,0.0f }, 1);
+		mesh.TranslateMesh(glm::vec3{ 1.0f,0.5f,0.0f }, 2);
+
+		mesh.SetTexture(texture);
+
+		mesh.ComputeMesh();		// Send to the GPU
+		mesh.FreeArraysCPU();	// Free CPU Array
+
+		// colored shader (used to draw the triangle)
+		bh3d::Shader& shader = m_multiMeshes.m_shader;
+		shader.LoadRaw(bh3d::TinyShader::TEXTURE_VERTEX(), bh3d::TinyShader::TEXTURE_FRAGMENT());
+	}
+
 
 
 	void Init() override
@@ -40,6 +107,10 @@ public:
 		m_cube.m_shader.LoadRaw(bh3d::TinyShader::TEXTURE_VERTEX(), bh3d::TinyShader::TEXTURE_FRAGMENT());
 		m_cube.m_mesh.SetTexture(texture);
 		m_cube.Init();
+
+		InitTriangle();
+		InitMultiMeshes();
+
 	}
 
 	void Update() override  {
@@ -62,7 +133,9 @@ public:
 		//Scene display
 		{
 			m_observer.Draw();
-			m_cube.Draw(m_camera);
+			//m_cube.Draw(m_camera);
+			//m_triangle.Draw(m_camera);
+			m_multiMeshes.Draw(m_camera);
 		}
 	}
 
